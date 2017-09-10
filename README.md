389ed4ff509cbe8d6faf74d2ba473a07561fb162
Carlos Porras <drexlerux@gmail.com>
2017-09-09 22:13:44 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,58 @@
+Alert Widget for Yii 2
+=========
+- Alert widget based on bootstrap-sweetalert extension http://lipis.github.io/bootstrap-sweetalert
+
+Installation 
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist drexlerux/yii2-bootstrap-sweetalert "*"
+```
+
+or add
+
+```json
+"drexlerux/yii2-bootstrap-sweetalert": "*"
+```
+
+to the require section of your composer.json.
+
+Usage
+------------
+Once the extension is installed, simply add widget to your page as follows:
+
+```php
+echo \drexlerux\sweetalert\SweetAlert::widget([
+        'type' => \drexlerux\sweetalert\SweetAlert::TYPE_WARNING,
+        'options' => [
+            'title' => 'Warning message',
+            'text' => "Do you want to delete it?",
+            'confirmButtonText'  => "Yes, delete it!",
+            'cancelButtonText' =>  "No, cancel plx!"
+        ]
+]);
+```
+
+Overrid Confirm
+----------------
+SweetAlertAsset overrides javascript confirm dialog by default.
+If you want to keep default edit your config.php
+
+```php
+'assetManager' => [
+    'bundles' => [
+        'drexlerux\sweetalert\SweetAlertAsset' => [
+            'overrideConfirm' => false
+        ]
+    ]
+]
+```
+
+
+Alert Options 
+----------------
+You can find them on the [options page](http://lipis.github.io/bootstrap-sweetalert/)
---
+++ SweetAlertAsset.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace drexlerux\sweetalert;
+
+use Yii;
+use yii\web\AssetBundle;
+use yii\web\View;
+
+/**
+ * Class SweetAlertAsset
+ */
+class SweetAlertAsset extends AssetBundle
+{
+
+    /**
+     * @var string the directory that contains the source asset files for this asset bundle.
+     * A source asset file is a file that is part of your source code repository of your Web application.
+     */
+    public $sourcePath = '@bower/bootstrap-sweetalert/lib';
+
+    /**
+     * @var array list of JavaScript files that this bundle contains. Each JavaScript file can be
+     * specified in one of the following formats:
+     */
+    public $js = [
+        ['sweet-alert.js', 'position' => View::POS_HEAD]
+    ];
+
+    /**
+     * @var array list of CSS files that this bundle contains. Each CSS file can be specified
+     * in one of the three formats as explained in [[js]].
+     */
+    public $css = [
+        'sweet-alert.css'
+    ];
+    
+    public $depends = [
+        'yii\web\JqueryAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+    
+    public $overrideConfirm = true;
+    
+    public function init()
+    {
+        parent::init();
+        if ($this->overrideConfirm) {
+            self::overrideConfirm();
+        }
+    }
+
+    public static function overrideConfirm()
+    {
+        Yii::$app->view->registerJs('
+            // workaround for bootstrap modal
+            $.fn.modal.Constructor.prototype.enforceFocus = function () {};
+            yii.confirm = function (message, ok, cancel) {
+                swal({
+                    title: message,
+                    type: "warning",
+                    showCancelButton: true
+                },
+                function(isConfirm){   
+                    if (isConfirm) {
+                        !ok || ok();
+                    } else {
+                        !cancel || cancel();
+                    }
+                });
+            }
+        ');
+    }
+
+}
